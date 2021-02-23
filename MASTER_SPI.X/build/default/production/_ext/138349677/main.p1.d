6473ef57cec67210e165d917ac0e80bb5dfa6e4e
JPval18057 <78621543+JPval18057@users.noreply.github.com>
2021-02-23 14:14:44 -0600

    Master_Code
    Este es el código que controla el pic maestro, versión final

---
+++ MASTER_SPI.X/build/default/production/_ext/138349677/main.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/138349677/main.p1:  \
+C:/Users/cabal/MPLABXProjects/MASTER_SPI.X/main.c  \
+C:/Users/cabal/MPLABXProjects/MASTER_SPI.X/lcd.h 
---
+++ MASTER_SPI.X/spi.h
@@ -0,0 +1,90 @@
+ /*
+ * File            : spi.h
+ * Author          : Ligo George
+ * Company         : electroSome
+ * Project         : SPI Library for MPLAB XC8
+ * Microcontroller : PIC 16F877A
+ * Created on April 15, 2017, 5:59 PM
+ */
+
+#ifndef SPI_H
+#define	SPI_H
+
+//#include <xc.h> 
+
+typedef enum 
+{
+    SPI_MASTER_OSC_DIV4  = 0b00100000,
+    SPI_MASTER_OSC_DIV16 = 0b00100001,
+    SPI_MASTER_OSC_DIV64 = 0b00100010,
+    SPI_MASTER_TMR2      = 0b00100011,
+    SPI_SLAVE_SS_EN      = 0b00100100,
+    SPI_SLAVE_SS_DIS     = 0b00100101
+}Spi_Type;
+
+typedef enum
+{
+    SPI_DATA_SAMPLE_MIDDLE   = 0b00000000,
+    SPI_DATA_SAMPLE_END      = 0b10000000
+}Spi_Data_Sample;
+
+typedef enum
+{
+    SPI_CLOCK_IDLE_HIGH  = 0b00010000,
+    SPI_CLOCK_IDLE_LOW   = 0b00000000        
+}Spi_Clock_Idle;
+
+typedef enum
+{
+    SPI_IDLE_2_ACTIVE    = 0b00000000,
+    SPI_ACTIVE_2_IDLE    = 0b01000000
+}Spi_Transmit_Edge;
+
+
+void spiInit(Spi_Type, Spi_Data_Sample, Spi_Clock_Idle, Spi_Transmit_Edge);
+void spiWrite(char);
+unsigned spiDataReady();
+char spiRead();
+
+#endif	/* SPI_H */
+
+void spiInit(Spi_Type sType, Spi_Data_Sample sDataSample, Spi_Clock_Idle sClockIdle, Spi_Transmit_Edge sTransmitEdge)
+{
+    TRISC5 = 0;
+    if(sType & 0b00000100) //If Slave Mode
+    {
+        SSPSTAT = sTransmitEdge;
+        TRISC3 = 1;
+    }
+    else              //If Master Mode
+    {
+        SSPSTAT = sDataSample | sTransmitEdge;
+        TRISC3 = 0;
+    }
+    
+    SSPCON = sType | sClockIdle;
+}
+
+static void spiReceiveWait()
+{
+    while ( !SSPSTATbits.BF ); // Wait for Data Receive complete
+}
+
+void spiWrite(char dat)  //Write data to SPI bus
+{
+    SSPBUF = dat;
+}
+
+unsigned spiDataReady() //Check whether the data is ready to read
+{
+    if(SSPSTATbits.BF)
+        return 1;
+    else
+        return 0;
+}
+
+char spiRead() //REad the received data
+{
+    spiReceiveWait();        // wait until the all bits receive
+    return(SSPBUF); // read the received data from the buffer
+}
